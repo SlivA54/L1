d17d495c6e55b8a9a2faa62b86682dfcfbf67cc2
SlivA54 <gavrisevdanil765@gmail.com>
2025-12-02 20:26:59 +0700

    Message

--- app/src/main/java/com/example/l1/MainActivity.kt
+++ app/src/main/java/com/example/l1/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.l1.ui.theme.L1Theme
 
 class MainActivity : ComponentActivity() {
@@ -44,4 +46,13 @@ fun GreetingPreview() {
     L1Theme {
         Greeting("Compose")
     }
+}
+
+@Composable
+fun DemoText(message: String, fontSize: Float) {
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
