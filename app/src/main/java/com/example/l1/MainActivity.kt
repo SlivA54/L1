6d169a1625f5596fcfd736ce050e52e79c9bc6e1
SlivA54 <gavrisevdanil765@gmail.com>
2025-12-02 20:15:16 +0700

    Compose

--- app/src/main/java/com/example/l1/MainActivity.kt
+++ app/src/main/java/com/example/l1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             L1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Compose",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
